ad4545d53da7f84de6008968a04778c05972f651
Damerian Dark <newbmailz@gmail.com>
2021-08-18 16:54:51 -0500

    first commit

---
+++ js/myro.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+		// Individual Frames
+    $(".s0").click(function(){
+    	$("#gal-slide").carousel(0);
+    });
+    $(".s1").click(function(){
+    	$("#gal-slide").carousel(1);
+    });
+    $(".s2").click(function(){
+    	$("#gal-slide").carousel(2);
+	});
+	$(".s3").click(function(){
+    	$("#gal-slide").carousel(3);
+    });
+    $(".s4").click(function(){
+    	$("#gal-slide").carousel(4);
+    });
+    $(".s5").click(function(){
+    	$("#gal-slide").carousel(5);
+	});	
+	$(".s6").click(function(){
+    	$("#gal-slide").carousel(6);
+    });
+    $(".s7").click(function(){
+    	$("#gal-slide").carousel(7);
+    });
+   
+});
